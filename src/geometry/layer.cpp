395da76a0612356cc17b7d165637264de8fce8ee
MatanSilver <matansilver@gmail.com>
2017-08-20 17:14:48 -0400

    brought back to linesegment

--- src/geometry/layer.cpp
+++ src/geometry/layer.cpp
@@ -3,13 +3,13 @@
 #include "layer.hpp"
 
 namespace samurai {
-  layer::layer(std::list<std::shared_ptr<layersegment>> linelist) {
+  layer::layer(std::list<std::shared_ptr<linesegment>> linelist) {
     loops = linelist_to_loops(linelist);
   }
-  std::list<std::list<std::shared_ptr<layersegment>>> layer::get_loops() {
+  std::list<std::list<std::shared_ptr<linesegment>>> layer::get_loops() {
     return loops;
   }
-  std::list<std::list<std::shared_ptr<layersegment>>> layer::linelist_to_loops(std::list<std::shared_ptr<layersegment>>) {
-    
+  std::list<std::list<std::shared_ptr<linesegment>>> layer::linelist_to_loops(std::list<std::shared_ptr<linesegment>>) {
+
   }
 }
--- src/geometry/layer.hpp
+++ src/geometry/layer.hpp
@@ -5,10 +5,10 @@ namespace samurai {
   class linesegment;
   class layer {
   private:
-    std::list<std::list<std::shared_ptr<layersegment>>> loops;
-    std::list<std::list<std::shared_ptr<layersegment>>> linelist_to_loops(std::list<std::shared_ptr<layersegment>>);
+    std::list<std::list<std::shared_ptr<linesegment>>> loops;
+    std::list<std::list<std::shared_ptr<linesegment>>> linelist_to_loops(std::list<std::shared_ptr<linesegment>>);
   public:
-    layer(std::list<std::shared_ptr<layersegment>>);
-    std::list<std::list<std::shared_ptr<layersegment>>> get_loops();
+    layer(std::list<std::shared_ptr<linesegment>>);
+    std::list<std::list<std::shared_ptr<linesegment>>> get_loops();
   };
 }
