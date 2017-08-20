e72b2889b064bbd50115e1edec0a03087c1842ed
MatanSilver <matansilver@gmail.com>
2017-08-19 23:52:46 -0400

    added layer class

--- src/geometry/layer.cpp
+++ src/geometry/layer.cpp
@@ -1,1 +1,15 @@
+#include <list>
+#include "linesegment.hpp"
 #include "layer.hpp"
+
+namespace samurai {
+  layer::layer(std::list<std::shared_ptr<layersegment>> linelist) {
+    loops = linelist_to_loops(linelist);
+  }
+  std::list<std::list<std::shared_ptr<layersegment>>> layer::get_loops() {
+    return loops;
+  }
+  std::list<std::list<std::shared_ptr<layersegment>>> layer::linelist_to_loops(std::list<std::shared_ptr<layersegment>>) {
+    
+  }
+}
--- src/geometry/layer.hpp
+++ src/geometry/layer.hpp
@@ -1,2 +1,14 @@
 #pragma once
+#include <list>
 
+namespace samurai {
+  class linesegment;
+  class layer {
+  private:
+    std::list<std::list<std::shared_ptr<layersegment>>> loops;
+    std::list<std::list<std::shared_ptr<layersegment>>> linelist_to_loops(std::list<std::shared_ptr<layersegment>>);
+  public:
+    layer(std::list<std::shared_ptr<layersegment>>);
+    std::list<std::list<std::shared_ptr<layersegment>>> get_loops();
+  };
+}
