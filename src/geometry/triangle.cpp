87448d041fe58c09349733424c4e7e48d76daaac
MatanSilver <matansilver@gmail.com>
2017-08-20 17:16:16 -0400

    added intersects plane function

--- src/geometry/triangle.cpp
+++ src/geometry/triangle.cpp
@@ -52,4 +52,40 @@ namespace samurai {
     }
     return true;
   }
+  std::set<std::shared_ptr<vector>> triangle::intersect_plane(float z) {
+    if (this->vectors[0].get_point()[2] == z && this->vectors[1].get_point()[2] == z) {
+      return {this->vectors[0], this->vectors[1]};
+    } else if (this->vectors[1].get_point()[2] == z && this->vectors[2].get_point()[2] == z) {
+      return {this->vectors[1], this->vectors[2]};
+    } else if (this->vectors[0].get_point()[2] == z && this->vectors[2].get_point()[2] == z) {
+      return {this->vectors[0], this->vectors[2]};
+    } else {
+      std::shared_ptr<vector> va, vb, origin;
+      if (this->vectors[0].get_point()[2] <= z && this->vectors[1].get_point()[2] <= z && this->vectors[2].get_point()[2] > z) {
+        va = this->vectors[0];
+        vb = this->vectors[1];
+        origin = this->vectors[2];
+      } else if (this->vectors[1].get_point()[2] <= z && this->vectors[2].get_point()[2] <= z && this->vectors[0].get_point()[2] > z) {
+        va = this->vectors[1];
+        vb = this->vectors[2];
+        origin = this->vectors[0];
+      } else if (this->vectors[0].get_point()[2] <= z && this->vectors[2].get_point()[2] <= z && this->vectors[1].get_point()[2] > z) {
+        va = this->vectors[0];
+        vb = this->vectors[2];
+        origin = this->vectors[1];
+      }
+      float t1 = (z - va.get_point()[2]) / (origin.get_point()[2] - va.get_point()[2])
+      float t2 = (z - vb.get_point()[2]) / (origin.get_point()[2] - vb.get_point()[2])
+      float x1 = va.get_point()[0] + t1*(origin.get_point()[0]-va.get_point()[0])
+      float x2 = vb.get_point()[0] + t2*(origin.get_point()[0]-vb.get_point()[0])
+      float = va.get_point()[1] + t1*(origin.get_point()[1]-va.get_point()[1])
+      float = vb.get_point()[1] + t2*(origin.get_point()[1]-vb.get_point()[1])
+      std::array<float, 3> a1(x1, y1, z);
+      std::array<float, 3> a2(x2, y2, z);
+      std::shared_ptr<vector> v1 = std::make_shared<vector>(a1);
+      std::shared_ptr<vector> v2 = std::make_shared<vector>(a2);
+      return {v1, v2};
+    }
+
+  }
 }
